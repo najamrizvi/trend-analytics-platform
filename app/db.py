209c7f0afbd1898fb860d7df040d918ab9cd3b78
najamrizvi <najamrizviofficial@gmail.com>
2026-04-24 22:25:08 +0500

    add db safety checks

--- app/db.py
+++ app/db.py
@@ -1,11 +1,17 @@
 from pymongo import MongoClient
+import os
 
-# 🔴 Hardcode URI (temporary but guaranteed to work)
-MONGO_URI = "MONGO_URI"
+MONGO_URI = os.getenv("MONGO_URI")
 
-client = MongoClient(MONGO_URI)
+client = None
+db = None
+raw_collection = None
+processed_collection = None
 
-db = client["trend_db"]
-
-raw_collection = db["raw_data"]
-processed_collection = db["processed_data"]
+if MONGO_URI:
+    client = MongoClient(MONGO_URI)
+    db = client["trend_db"]
+    raw_collection = db["raw_data"]
+    processed_collection = db["processed_data"]
+else:
+    print("WARNING: MONGO_URI is not set")
