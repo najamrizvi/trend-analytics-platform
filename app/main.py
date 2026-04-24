8dc10ae78fc1ce91020502b2d4602e425a5e7936
najamrizvi <najamrizviofficial@gmail.com>
2026-04-24 15:59:19 +0500

    fix main.py syntax error

--- app/main.py
+++ app/main.py
@@ -1,21 +1,10 @@
-from dotenv import 
-from pathlib import Path
-import os
-
-# Force load .env from ROOT folder
-env_path = Path(__file__).resolve().parent.parent / ".env"
-load_dotenv(dotenv_path=env_path)
-
-print("Loaded MONGO:", os.getenv("mongodb+srv://najamrizviofficial_db_user:vum08exgHPC0Vs42@cluster0.f9a6nyl.mongodb.net/trend-analytics"))  # debug
-
 from fastapi import FastAPI
 from app.routes import router
 
 app = FastAPI()
 
-
 @app.get("/")
 def home():
-    return {"message": "API running 🚀"}
+    return {"message": "API is running 🚀"}
 
 app.include_router(router)
