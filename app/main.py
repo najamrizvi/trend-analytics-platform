110ef19d28b2e79a84d5547afed9f343ee3e5f34
najamrizvi <najamrizviofficial@gmail.com>
2026-04-24 16:05:24 +0500

    improve api metadata

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from app.routes import router
 
-app = FastAPI()
+app = FastAPI(
+    title="Trend Analytics API",
+    description="AI-powered market intelligence platform",
+    version="1.0.0"
+)
+
+    
 
 @app.get("/")
 def home():
